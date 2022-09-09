75ef623768deffcd26849b29abb735e9f27caa70
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-10 02:19:55 +0300

    Image and Input Button Test

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ fetch("https://dog.ceo/api/breeds/image/random")
     var source = data.message;
     document.getElementById("Image").src = source;
   });
+
+function nameInput() {
+  console.log("Hello From Input");
+}
