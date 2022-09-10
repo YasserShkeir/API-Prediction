a65df14f85890fdec3b9372ae213e714bc8ad85b
Yasser Shkeir <shkeiryasser@gmail.com>
2022-09-11 00:57:17 +0300

    Mobile Design Improvement + Final Comments & Edits

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Used to fetch the url of a dog image
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((response) => response.json())
   .then((data) => {
@@ -5,12 +6,23 @@ fetch("https://dog.ceo/api/breeds/image/random")
     document.getElementById("Image").src = source;
   });
 
+// This function is called when the user clicks on the input section button
+// It uses fetch from 3 APIs
 function nameInput() {
+  // Element with id message displays the below if the user inputs a name,
+  // no need for an 'if' condition
   document.getElementById("Message").innerHTML = "Your Results Below:";
+
+  // create a var name to store the name sent from input
   var name = document.getElementById("input").value;
+
+  // if name is not null or a space
   if (name && name != " ") {
+    // **** START GENDERIZE SECTION ****
+    // Create two vars, gender and proba, to store the latter
     var gen_gender;
     var gen_proba;
+    // Call fetch, and store the values of gender and proba
     fetch("https://api.genderize.io/?name=" + name)
       .then((response) => response.json())
       .then((data) => {
@@ -21,17 +33,21 @@ function nameInput() {
         ).innerHTML = `Chance of being ${gen_gender} is ${gen_proba * 100}%`;
       });
 
+    // **** START AGIFY SECTION ****
+    // Create vars agi_age to store the age
     var agi_age;
-    var agi_count;
+    // Call fetch and store the value of age
     fetch("https://api.agify.io/?name=" + name)
       .then((response) => response.json())
       .then((data) => {
         agi_age = data.age;
-        agi_count = data.count;
         document.getElementById("Age").innerHTML = `Age is ${agi_age}`;
       });
 
+    // **** START NATIONALIZE SECTION ****
+    // Create cty_country to store the countries dictionary
     var cty_country;
+    // Create two arrays to store country IDs and the proba for each (Easier to loop through next)
     var arr_ids = [];
     var arr_proba = [];
     fetch("https://api.nationalize.io/?name=" + name)
@@ -53,10 +69,11 @@ function nameInput() {
         document.getElementById("Nationality").innerHTML = nationality;
       });
   } else {
+    // If the input is invalid, change message to waiting, empty all result divs, and alert user
     document.getElementById("Message").innerHTML = "Waiting for input...";
     document.getElementById("Gender").innerHTML = "";
     document.getElementById("Age").innerHTML = "";
     document.getElementById("Nationality").innerHTML = "";
-    alert("Please input a valid name");
+    alert("WOOF WOOF!\nPlease input a valid name");
   }
 }
